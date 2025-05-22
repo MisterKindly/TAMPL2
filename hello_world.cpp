b7fb86c56a15dc0173a65c1cb9ec60e842e2ba9d
MisterKindly <9154884415@inbox.ru>
2025-05-22 18:28:49 +0300

    Убрал using namespace std

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
-using namespace std;
 
 int main()
 {
-string name;
-cout << "Введите ваше имя";
-cin << name;
-cout << "Hello World from " << name << endl;
-return(0)
-;}
+    std::string name;
+    std::cout << "Enter your name: ";
+    std::cin >> name;
+    std::cout << "Hello world from " << name << std::endl;
+    return 0;
+}
