75e915c87b786ab51f212b18686c4cf2cd4af9b7
Temnuy <9154884415@inbox.ru>
2025-05-22 19:44:25 +0300

    Поменял комментарии

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,8 +3,8 @@
 int main()
 {
     std::string name; //переменная для имени пользователя
-    std::cout << "Enter your name: "; //просьба ввести имя
-    std::cin >> name;
-    std::cout << "Hello world from " << name << std::endl; //вывод Hello world from <имя пользователя>
+    std::cout << "Enter your name: "; //вывод: Enter your name: 
+    std::cin >> name; // ввод имени пользователя
+    std::cout << "Hello world from " << name << std::endl; //вывод: Hello world from <имя пользователя>
     return 0;
 }
